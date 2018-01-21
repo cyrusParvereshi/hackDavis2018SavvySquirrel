d0d33ac38cae781614b7746fe97f1a01e3b22346
Cyrus Parvereshi <cyrusParvereshi@users.noreply.github.com>
2018-01-20 22:04:56 -0800

    more php

---
+++ php/php2.php
@@ -0,0 +1,29 @@
+<?php
+?>
+
+<html>
+
+<!-------------------------------------------------------------Head--------->
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<title>Saavy Squirrel v2</title>
+    <link rel = "stylesheet" type = "text/css" href = "./css/text.css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script type="text/javascript" src="./js/script.js"></script>
+    <script src="https://cdn.snipcart.com/scripts/2.0/snipcart.js" id="snipcart" data-api-key="ZDE3YmE2NmUtNzA5ZS00ZjEyLWEzOGMtOWE3MmYzYjZhN2MxNjM2MzUxNDY3NTE3MjIwODYw"></script>
+
+
+</head>
+
+<body>
+
+<?php
+
+// ------------------------------------------------------------Copy Right Footer---
+
+function doCopyRight()  {
+	echo "&copy; 2010-".date("Y"). " Wheless Farms";
+}
+
+?>
